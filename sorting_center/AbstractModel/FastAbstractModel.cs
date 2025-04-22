42af533c296844cc3c7cf7f932dd5a4eb583e1f0
TARAS AFANASENKO <tsafanasenko@icloud.com>
2025-04-22 15:23:28 +0300

    abstract_model

---
+++ sorting_center/AbstractModel/FastAbstractModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace AbstractModel
+{
+
+   
+
+    public abstract class FastAbstractObject
+    {
+        public string uid = Guid.NewGuid().ToString();
+        [XmlElement(Type = typeof(TimeSpan))]
+        public TimeSpan lastUpdated;
+        public abstract (TimeSpan, FastAbstractEvent) getNearestEvent();
+        public abstract void Update(TimeSpan timeSpan);
+
+        public int uidi
+        {
+            get
+            {
+                return int.Parse(uid);
+            }
+        }
+    }
+
+    public abstract class FastAbstractEvent
+    {
+        public string objId = null;
+        public abstract void runEvent(FastAbstractWrapper wrapper, TimeSpan timeSpan);
+    }
+
+    public abstract class FastAbstractWrapper
+    {
+        public Dictionary<string, FastAbstractObject> objects = new Dictionary<string, FastAbstractObject>();
+        
+        protected SortedList<TimeSpan, FastAbstractEvent> eventList = new SortedList<TimeSpan, FastAbstractEvent>();
+        protected Dictionary<string, TimeSpan> objectsEventTime = new Dictionary<string, TimeSpan>();
+        public TimeSpan updatedTime;
+        private HashSet<string> objectsKeyForUpdate;
+
+        public Action<string> writeDebug = Console.WriteLine;
+        public Action<string> writeError = Console.WriteLine;
+
+        public bool isDebug = false;
+
+        public void WriteDebug(string debug)
+        {
+            if (isDebug)
+                writeDebug(debug);
+        }
+
+        public void RemoveObjects(string uid)
+        {
+            objects.Remove(uid);
+            objectsEventTime.Remove(uid);
+            objectsKeyForUpdate.Remove(uid);
+        }
+
+        public List<string> getObjectKeys()
+        {
+            return objects.Keys.ToList();
+        }
+
+        public FastAbstractObject getObject(string key)
+        {
+            var obj = objects[key];
+            obj.Update(updatedTime);
+            objectsKeyForUpdate.Add(key);
+            return objects[key];
+        }
+
+        protected TimeSpan AddEvent(TimeSpan timeSpan, FastAbstractEvent modelEvent, string objUid = null)
+        {
+            if (timeSpan < updatedTime)
+                throw new Exception("Time error!!");
+            while (eventList.ContainsKey(timeSpan))
+            {
+                timeSpan = timeSpan.Add(TimeSpan.FromTicks(1));
+            }
+            modelEvent.objId = objUid;
+            eventList.Add(timeSpan, modelEvent);
+            if (!(objUid is null))
+            {
+                objectsEventTime.Add(objUid, timeSpan);
+            }
+            return timeSpan;
+        }
+
+        public bool Next()
+        {
+            if (eventList.Count == 0)
+                return false;
+            objectsKeyForUpdate = new HashSet<string>();
+            var task = eventList.First();
+            updatedTime = task.Key;
+            eventList.Remove(task.Key);
+            if (!(task.Value.objId is null))
+            {
+                getObject(task.Value.objId);
+            }
+            //Console.WriteLine($"{task.Key} {task.Value.GetType()}");
+            task.Value.runEvent(this, task.Key);
+            foreach (var objKey in objectsKeyForUpdate)
+            {
+                if (objectsEventTime.ContainsKey(objKey)) {
+                    eventList.Remove(objectsEventTime[objKey]);
+                    objectsEventTime.Remove(objKey);
+                }
+                var ev = objects[objKey].getNearestEvent();
+                if (!(ev.Item2 is null))
+                {
+                    AddEvent(ev.Item1, ev.Item2, objKey);
+                }
+            }
+
+            return true;
+        }
+
+        public void addObject(FastAbstractObject obj)
+        {
+            objects.Add(obj.uid, obj);
+            var ev = obj.getNearestEvent();
+            if (!(ev.Item2 is null))
+            {
+                AddEvent(ev.Item1, ev.Item2, obj.uid);
+            }
+        }
+    }
+
+
+}
---
+++ sorting_center/AbstractModel/Helper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace AbstractModel
+{
+    public class Helper
+    {
+        public static byte[] SerializeXML<T>(T stamp)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(T));
+            MemoryStream stream = new MemoryStream();
+            formatter.Serialize(stream, stamp);
+            return stream.ToArray();
+        }
+
+
+
+        public static void SerializeXMLToFile<T>(T stamp, string filePath)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(T));
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                formatter.Serialize(fileStream, stamp);
+            }
+        }
+
+        public static T DeserializeXMLFromFile<T>(string filePath)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(T));
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                return (T)formatter.Deserialize(fileStream);
+            }
+        }
+
+        public static T DeserializeXML<T>(byte[] binaryData)
+        {
+            var formatter = new XmlSerializer(typeof(T));
+            var ms = new MemoryStream(binaryData);
+            return (T)formatter.Deserialize(ms);
+        }
+
+    }
+}
