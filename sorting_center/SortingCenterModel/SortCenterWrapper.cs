234bbb0f39a422d45b227a2b9c13813c38093fed
TARAS AFANASENKO <tsafanasenko@icloud.com>
2025-04-22 15:47:23 +0300

    skelet

---
+++ sorting_center/SortingCenterModel/SortCenterWrapper.cs
@@ -0,0 +1,14 @@
+﻿using AbstractModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingCenterModel
+{
+    public class SortCenterWrapper : FastAbstractWrapper
+    {
+
+    }
+}
---
+++ sorting_center/SortingCenterRuner/SortRunner.cs
@@ -0,0 +1,19 @@
+﻿using SortingCenterModel;
+
+namespace sorting_center_runer
+{
+    class SortRunner
+    {
+
+
+        static void Main(string[] args)
+        {
+            SortCenterWrapper wrapper = new SortCenterWrapper();
+            while (wrapper.Next())
+            {
+
+            }
+        }
+    }
+}
+
